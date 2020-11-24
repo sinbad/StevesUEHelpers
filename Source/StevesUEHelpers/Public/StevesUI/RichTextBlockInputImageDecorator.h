1904476cc48c1554dc63cbcaeddbd6012c771bc2
Steve Streeting <steve@stevestreeting.com>
2020-11-24 12:00:27 +0000

    Cannot support custom theme in decorator, UE provides no UI to edit properties of decorators

--- Source/StevesUEHelpers/Public/StevesUI/RichTextBlockInputImageDecorator.h
+++ Source/StevesUEHelpers/Public/StevesUI/RichTextBlockInputImageDecorator.h
@@ -11,15 +11,9 @@ UCLASS()
 class STEVESUEHELPERS_API URichTextBlockInputImageDecorator : public URichTextBlockDecorator
 {
     GENERATED_BODY()
-
-protected:
-    /// Custom theme to use for this input image set; if not supplied will use UStevesGameSubsystem::DefaultUiTheme
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)   
-    UUiTheme* CustomTheme;
-    
+  
 public:
 
     virtual TSharedPtr<ITextDecorator> CreateDecorator(URichTextBlock* InOwner) override;
 
-    virtual UUiTheme* GetCustomTheme() const {  return CustomTheme; } 
 };
