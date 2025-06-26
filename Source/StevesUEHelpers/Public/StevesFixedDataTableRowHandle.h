3bd8d8d58efc0e065aabb8453eebcbae8e2f171f
Steve Streeting <steve@stevestreeting.com>
2025-06-26 14:47:27 +0100

    Add better docs to FStevesFixedDataTableRowHandle header

--- Source/StevesUEHelpers/Public/StevesFixedDataTableRowHandle.h
+++ Source/StevesUEHelpers/Public/StevesFixedDataTableRowHandle.h
@@ -4,6 +4,11 @@
 #include "StevesFixedDataTableRowHandle.generated.h"
 
 /// Just a type to denote that this table row handle should be edited differently
+/// 
+/// Usage:
+///    UPROPERTY(EditAnywhere, meta=(DataTable="/Game/Data/DT_YourData"))
+///    FStevesFixedDataTableRowHandle Row;
+///    
 USTRUCT(BlueprintType)
 struct STEVESUEHELPERS_API FStevesFixedDataTableRowHandle : public FDataTableRowHandle
 {
