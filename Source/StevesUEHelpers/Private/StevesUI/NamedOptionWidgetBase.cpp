4d6cf469f455c2713eb6076461b4c1f6f4fb88e6
Steve Streeting <steve@stevestreeting.com>
2025-08-13 17:50:45 +0100

    Add NamedOptionWidgetBase

---
+++ Source/StevesUEHelpers/Private/StevesUI/NamedOptionWidgetBase.cpp
@@ -0,0 +1,79 @@
+﻿// 
+
+
+#include "StevesUI/NamedOptionWidgetBase.h"
+
+#include "StevesUI.h"
+
+void UNamedOptionWidgetBase::ClearOptions()
+{
+	Super::ClearOptions();
+
+	NameIndex.Reset();
+}
+
+int UNamedOptionWidgetBase::AddNamedOption(FName Name, FText Option)
+{
+	int Idx = Super::AddOption(Option);
+	NameIndex.Add(Name, Idx);
+	return Idx;
+}
+
+void UNamedOptionWidgetBase::SetOptions(const TArray<FText>& NewOptions, int NewSelectedIndex)
+{
+	Super::SetOptions(NewOptions, NewSelectedIndex);
+
+
+	UE_LOG(LogStevesUI, Warning, TEXT("Using SetOptions on a Named Option Widget loses the name lookup index, consider using SetNamedOptions instead"))
+	NameIndex.Reset();
+}
+
+void UNamedOptionWidgetBase::SetNamedOptions(const TMap<FName, FText>& InOptions,
+                                             FName NewSelectedName)
+{
+	Options.Reset();
+	NameIndex.Reset();
+	for (const auto& Pair : InOptions)
+	{
+		const int Idx = Options.Add(Pair.Value);
+		NameIndex.Add(Pair.Key, Idx);
+	}
+	SetSelectedName(NewSelectedName);
+}
+
+FName UNamedOptionWidgetBase::GetSelectedName() const
+{
+	return SelectedName;
+}
+
+int UNamedOptionWidgetBase::GetIndexForName(FName Name)
+{
+	if (auto pIdx = NameIndex.Find(Name))
+	{
+		return *pIdx;
+	}
+	return INDEX_NONE;
+}
+
+FName UNamedOptionWidgetBase::GetNameForIndex(int Index)
+{
+	if (auto pName = NameIndex.FindKey(Index))
+	{
+		return *pName;
+	}
+	return NAME_None;
+}
+
+void UNamedOptionWidgetBase::SetSelectedIndex(int NewIndex)
+{
+	// Update which name this is
+	// Do this first so that the event called in the superclass has this data as well
+	SelectedName = GetNameForIndex(NewIndex);
+	
+	Super::SetSelectedIndex(NewIndex);
+}
+
+void UNamedOptionWidgetBase::SetSelectedName(FName Name)
+{
+	SetSelectedIndex(GetIndexForName(Name));
+}
---
+++ Source/StevesUEHelpers/Public/StevesUI/NamedOptionWidgetBase.h
@@ -0,0 +1,61 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OptionWidgetBase.h"
+#include "NamedOptionWidgetBase.generated.h"
+
+/**
+ * Subclass of UOptionWidget base to provide convenient access via FNames, on top of the regular integers.
+ * Unlike UOptionWidgetBase, this widget can only be populated in code, you can't set up the options
+ * and the default selection in the designer.
+ */
+UCLASS()
+class STEVESUEHELPERS_API UNamedOptionWidgetBase : public UOptionWidgetBase
+{
+	GENERATED_BODY()
+protected:
+	TMap<FName, int> NameIndex;
+	FName SelectedName = NAME_None;
+
+public:
+	virtual void ClearOptions() override;
+	/**
+	 * @brief Adds a new named option
+	 * @param Name The name to identify the option by
+	 * @param Option The text for the new option
+	 * @return The index for the new option
+	 */
+	UFUNCTION(BlueprintCallable, Category="NamedOptionWidget")
+	virtual int AddNamedOption(FName Name, FText Option);
+
+
+	virtual void SetOptions(const TArray<FText>& Options, int NewSelectedIndex = 0) override;
+	/**
+     * @brief Sets all of the named options available for this control
+     * @param NewOptions All options to be available, indexed by name
+     * @param NewSelectedName Which of the options to select by default
+     */
+    UFUNCTION(BlueprintCallable, Category="NamedOptionWidget")
+    virtual void SetNamedOptions(const TMap<FName, FText>& NewOptions, FName NewSelectedName = NAME_None);
+    
+    UFUNCTION(BlueprintPure, Category="NamedOptionWidget")
+    virtual FName GetSelectedName() const;
+
+	/// Get the index in this option list corresponding to a name
+    UFUNCTION(BlueprintCallable, Category="NamedOptionWidget")
+	virtual int GetIndexForName(FName Name);
+
+	/// Get the name in this option list corresponding to an index
+	UFUNCTION(BlueprintCallable, Category="NamedOptionWidget")
+	virtual FName GetNameForIndex(int Index);
+
+	virtual void SetSelectedIndex(int NewIndex) override;
+	/**
+     * @brief Change the selected option by name
+     * @param Name The name of the new option to set, can be None for no selection
+     */
+    UFUNCTION(BlueprintCallable, Category="NamedOptionWidget")
+    virtual void SetSelectedName(FName Name);
+};
