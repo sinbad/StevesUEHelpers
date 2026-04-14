33408f5d2dd320c6642724dc09a9440cbbdcb94f
Steve Streeting <steve@stevestreeting.com>
2026-04-14 15:44:49 +0100

    Added STEVES_LOG_ONCE and STEVES_LOG_NOREPEAT

---
+++ Source/StevesUEHelpers/Public/StevesLogHelpers.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+/// Log once per session only 
+#define STEVES_LOG_ONCE(CategoryName, Verbosity, Format, ...) \
+{ \
+	static bool bLogged = false; \
+	UE_CLOG(!bLogged, CategoryName, Verbosity, Format, ##__VA_ARGS__); \
+	bLogged = true; \
+}
+
+/// Suppress repeat logs if the value is the same as last time at the call site
+#define STEVES_LOG_NOREPEAT(TypeName, Value, CategoryName, Verbosity, Format, ...) \
+{ \
+	static TOptional<TypeName> LastValue; \
+	UE_CLOG(!LastValue.IsSet() || LastValue != Value, CategoryName, Verbosity, Format, ##__VA_ARGS__); \
+	LastValue = Value; \
+}
